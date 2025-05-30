66c3d8c2a34bb815c23595287eb559e09b6ffbd6
Nathan <nathan.torresc@gmail.com>
2025-05-30 10:03:00 -0700

    Implementacion rama dev nate

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,7 +3,7 @@ import { LoginForm, LoginHeader, LoginIllustration, LoginLogo } from '@/componen
 export default function LoginPage() {
   return (
     <div className="flex min-h-screen w-full flex-col md:grid md:grid-cols-2">
-      <div className="flex flex-col items-center justify-center bg-white px-6 py-10 md:px-12 lg:px-20">
+      <div className="flex flex-col items-center justify-center bg-white px-6 py-10 text-white md:px-12 lg:px-20">
         <div className="w-full max-w-[470px]">
           <LoginLogo />
           <LoginHeader />
